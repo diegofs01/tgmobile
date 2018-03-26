68ab9ee3676f1a18c5177633842e47033d3d3ee9
Diego Ferreira <diegofs01@hotmail.com>
2018-03-26 15:13:37 -0300

    Change layout and add dialog alert in Veiculo-Consulta

--- src/pages/veiculo-consulta/veiculo-consulta.ts
+++ src/pages/veiculo-consulta/veiculo-consulta.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Dialogs } from '@ionic-native/dialogs';
 
 import { Aluno } from '../../model/aluno';
 import { Veiculo } from '../../model/veiculo';
@@ -32,30 +33,38 @@ export class VeiculoConsultaPage {
     public navParams: NavParams, 
     public veiculoService: VeiculoServiceProvider,
     public alunoService: AlunoServiceProvider,
-    public cursoService: CursoServiceProvider) {
+    public cursoService: CursoServiceProvider,
+    private dialogs: Dialogs) {
 
-      this.veiculo = new Veiculo('','','', 0, 0,'','','');
-      this.aluno = new Aluno('','','','','',0,'','','','','','','', 0);
-      this.curso = new Curso(0,'');
+      this.veiculo = {} as Veiculo;
+      this.aluno = {} as Aluno;
+      this.curso = {} as Curso;
   }
 
   consultarVeiculo() {
-    if(this.placa !== undefined && this.placa !== '') {
+    if(this.placa !== undefined && this.placa !== '' && this.placa.length === 8) {
+      this.placa = this.placa.toUpperCase();
+
       this.veiculoService.buscar(this.placa)
       .then(data => {
         this.veiculo = data;
 
-        this.alunoService.buscar(this.veiculo.raAluno)
-        .then(data => {
-          this.aluno = data;
-
-          this.cursoService.buscar(this.aluno.idCurso)
+        if(this.veiculo.placa !== null) {
+          this.alunoService.buscar(this.veiculo.raAluno)
           .then(data => {
-            this.curso = data;
-          });
-          
-        });
-        
+
+            this.aluno = data;
+            this.cursoService.buscar(this.aluno.idCurso)
+            .then(data => {
+              this.curso = data;
+            });
+            
+          }); 
+        } else {
+          this.dialogs.alert('Veículo não cadastrado', 'Erro');
+          this.aluno = {} as Aluno;
+          this.curso = {} as Curso;
+        }
       });
     }
   }
